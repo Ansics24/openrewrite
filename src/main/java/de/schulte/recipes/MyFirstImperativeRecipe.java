f2b0e13321a3f7150235cd8f922b2f69384c9dcf
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-21 14:42:17 +0100

    Imperative recipes - Testing

--- src/main/java/de/schulte/recipes/MyFirstImperativeRecipe.java
+++ src/main/java/de/schulte/recipes/MyFirstImperativeRecipe.java
@@ -13,7 +13,7 @@ public class MyFirstImperativeRecipe extends Recipe {
 
     @Override
     public @NlsRewrite.Description String getDescription() {
-        return "The first imperative recipe as a showcase for the OpenRewrite course";
+        return "The first imperative recipe as a showcase for the OpenRewrite course.";
     }
 
     @Override
