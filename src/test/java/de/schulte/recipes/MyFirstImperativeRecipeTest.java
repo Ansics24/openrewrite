c81a80a2fa38e36333a7b02402a3b5280161145e
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-26 14:46:50 +0100

    Imperative recipes - Changing - working example
    (cherry picked from commit 4a58878754a10758a37c21617e04f0df66622af9)

--- src/test/java/de/schulte/recipes/MyFirstImperativeRecipeTest.java
+++ src/test/java/de/schulte/recipes/MyFirstImperativeRecipeTest.java
@@ -5,8 +5,6 @@ import org.openrewrite.java.Assertions;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MyFirstImperativeRecipeTest implements RewriteTest {
 
     @Override
@@ -18,7 +16,22 @@ class MyFirstImperativeRecipeTest implements RewriteTest {
     void myFirstTest() {
         rewriteRun(Assertions.java(
                 """
-                        public class Foo {}
+                        public class Foo {
+                        
+                          public String foo() {
+                            return "Hello";
+                          }
+                        
+                        }
+                        """,
+                """
+                        public class Foo {
+                        
+                          public String greet(String name) {
+                            return "Hello";
+                          }
+                        
+                        }
                         """
         ));
     }
