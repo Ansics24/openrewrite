2cfb735ab6b6500bb2073d1a9e01ed70d55fc0e5
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-26 14:57:52 +0100

    Imperative recipes - Datasharing
    (cherry picked from commit 865b795bf9f0aa619003d0b04eefd62446b85e50)

--- src/main/java/de/schulte/recipes/MyfirstVisitor.java
+++ src/main/java/de/schulte/recipes/MyfirstVisitor.java
@@ -1,21 +1,33 @@
 package de.schulte.recipes;
 
+import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
 
 public class MyfirstVisitor extends JavaIsoVisitor<ExecutionContext> {
 
+    public static final String MESSAGE_NAME_UNPERSONAL_GREETING = "unpersonalGreeting";
+
     @Override
     public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
-        System.out.printf("Visiting method %s%n", method.getName());
-        return super.visitMethodDeclaration(method, executionContext);
+        final var methodDeclaration = super.visitMethodDeclaration(method, executionContext);
+        if (isUnpersonalGreeting(getCursor())) {
+            // TODO return adjustment
+        }
+        return methodDeclaration;
+    }
+
+    private boolean isUnpersonalGreeting(Cursor methodCursor) {
+        return methodCursor.getMessage(MESSAGE_NAME_UNPERSONAL_GREETING, false);
     }
 
     @Override
-    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
-        final var result = super.visitClassDeclaration(classDecl, executionContext);
-        System.out.printf("Visited class %s%n", classDecl.getName().getSimpleName());
-        return result;
+    public J.Literal visitLiteral(J.Literal literal, ExecutionContext executionContext) {
+        if (literal.getValue() instanceof String || literal.getValue().toString().equals("Hello")) {
+            final var methodCursor = getCursor().dropParentUntil(node -> node instanceof J.MethodDeclaration);
+            methodCursor.putMessage(MESSAGE_NAME_UNPERSONAL_GREETING, true);
+        }
+        return super.visitLiteral(literal, executionContext);
     }
 }
