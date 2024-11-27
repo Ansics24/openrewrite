df072334361e73f2f712b739564b0384ed17b192
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-27 11:42:31 +0100

    Imperative recipes - changing method argument and name

--- src/main/java/de/schulte/recipes/MyfirstVisitor.java
+++ src/main/java/de/schulte/recipes/MyfirstVisitor.java
@@ -3,17 +3,21 @@ package de.schulte.recipes;
 import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.tree.J;
 
 public class MyfirstVisitor extends JavaIsoVisitor<ExecutionContext> {
 
     public static final String MESSAGE_NAME_UNPERSONAL_GREETING = "unpersonalGreeting";
 
+    private JavaTemplate addParameterTemplate = JavaTemplate.builder("String name").build();
+
     @Override
     public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
         final var methodDeclaration = super.visitMethodDeclaration(method, executionContext);
         if (isUnpersonalGreeting(getCursor())) {
-            // TODO return adjustment
+            final J.MethodDeclaration newMethodDeclaration = addParameterTemplate.apply(getCursor(), method.getCoordinates().replaceParameters());
+            return newMethodDeclaration.withName(method.getName().withSimpleName("greet"));
         }
         return methodDeclaration;
     }
@@ -26,7 +30,10 @@ public class MyfirstVisitor extends JavaIsoVisitor<ExecutionContext> {
     public J.Literal visitLiteral(J.Literal literal, ExecutionContext executionContext) {
         if (literal.getValue() instanceof String || literal.getValue().toString().equals("Hello")) {
             final var methodCursor = getCursor().dropParentUntil(node -> node instanceof J.MethodDeclaration);
-            methodCursor.putMessage(MESSAGE_NAME_UNPERSONAL_GREETING, true);
+            final J.MethodDeclaration method = methodCursor.getValue();
+            if(!method.getName().getSimpleName().equals("greet")) {
+                methodCursor.putMessage(MESSAGE_NAME_UNPERSONAL_GREETING, true);
+            }
         }
         return super.visitLiteral(literal, executionContext);
     }
