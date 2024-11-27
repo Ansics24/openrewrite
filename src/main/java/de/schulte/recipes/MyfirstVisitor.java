76e77068fdb7fa0155f9b8c90e5e0b4de6542d5a
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-27 14:26:20 +0100

    Imperative recipes -  non-isomorphic visitors
    (cherry picked from commit 79ae68d956f36fa5c94f314226b6957167248228)

--- src/main/java/de/schulte/recipes/MyfirstVisitor.java
+++ src/main/java/de/schulte/recipes/MyfirstVisitor.java
@@ -4,20 +4,37 @@ import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.Space;
+import org.openrewrite.marker.Markers;
 
-public class MyfirstVisitor extends JavaIsoVisitor<ExecutionContext> {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.UUID;
+
+public class MyfirstVisitor extends JavaVisitor<ExecutionContext> {
 
     public static final String MESSAGE_NAME_UNPERSONAL_GREETING = "unpersonalGreeting";
 
     private JavaTemplate addParameterTemplate = JavaTemplate.builder("String name").build();
 
+    private JavaTemplate personalGreetingTemplate = JavaTemplate.builder("\"Hello %s\".formatted(name)").build();
+
     @Override
-    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
+    public J visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
         final var methodDeclaration = super.visitMethodDeclaration(method, executionContext);
         if (isUnpersonalGreeting(getCursor())) {
-            final J.MethodDeclaration newMethodDeclaration = addParameterTemplate.apply(getCursor(), method.getCoordinates().replaceParameters());
-            return newMethodDeclaration.withName(method.getName().withSimpleName("greet"));
+            final J.MethodDeclaration newMethodDeclaration = addParameterTemplate.apply(new Cursor(getCursor(),
+                            methodDeclaration),
+                    method.getCoordinates().replaceParameters());
+
+            final var modifiers = new ArrayList<>(newMethodDeclaration.getModifiers());
+            modifiers.add(new J.Modifier(UUID.randomUUID(), Space.SINGLE_SPACE, Markers.EMPTY, "final",
+                    J.Modifier.Type.Final, Collections.emptyList()));
+
+            return newMethodDeclaration.withName(method.getName().withSimpleName("greet")).withModifiers(modifiers)
+                                       .withMethodType(newMethodDeclaration.getMethodType().withName("greet"));
         }
         return methodDeclaration;
     }
@@ -27,12 +44,13 @@ public class MyfirstVisitor extends JavaIsoVisitor<ExecutionContext> {
     }
 
     @Override
-    public J.Literal visitLiteral(J.Literal literal, ExecutionContext executionContext) {
+    public J visitLiteral(J.Literal literal, ExecutionContext executionContext) {
         if (literal.getValue() instanceof String || literal.getValue().toString().equals("Hello")) {
             final var methodCursor = getCursor().dropParentUntil(node -> node instanceof J.MethodDeclaration);
             final J.MethodDeclaration method = methodCursor.getValue();
-            if(!method.getName().getSimpleName().equals("greet")) {
+            if (!method.getName().getSimpleName().equals("greet")) {
                 methodCursor.putMessage(MESSAGE_NAME_UNPERSONAL_GREETING, true);
+                return personalGreetingTemplate.apply(getCursor(), literal.getCoordinates().replace());
             }
         }
         return super.visitLiteral(literal, executionContext);
