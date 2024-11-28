59973e15b6ab70f0fa493477e4630a65afa1b666
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-11-28 13:50:10 +0100

    Imperative recipes - configuration options

--- src/main/java/de/schulte/recipes/MyFirstImperativeRecipe.java
+++ src/main/java/de/schulte/recipes/MyFirstImperativeRecipe.java
@@ -1,11 +1,20 @@
 package de.schulte.recipes;
 
-import org.openrewrite.ExecutionContext;
-import org.openrewrite.NlsRewrite;
-import org.openrewrite.Recipe;
-import org.openrewrite.TreeVisitor;
+import org.openrewrite.*;
 
 public class MyFirstImperativeRecipe extends Recipe {
+
+    @Option(displayName = "GreetingMethodName", description = "The name of the method greeting personally", required = false)
+    private String greetingMethodName;
+
+    public MyFirstImperativeRecipe() {
+        this(null);
+    }
+
+    public MyFirstImperativeRecipe(String greetingMethodName) {
+        this.greetingMethodName = greetingMethodName;
+    }
+
     @Override
     public @NlsRewrite.DisplayName String getDisplayName() {
         return "My first imperative recipe";
@@ -18,6 +27,6 @@ public class MyFirstImperativeRecipe extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new MyfirstVisitor();
+        return new MyfirstVisitor(greetingMethodName);
     }
 }
